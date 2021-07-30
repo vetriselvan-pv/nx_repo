722e77d08ed36c91cd4f8ca0e1d014cefd174ff6
Vetriselvan-Panneerselvam <vetriselvan.panneerselvam@newage-global.com>
2021-07-30 14:47:36 +0530

    feat(angular): angular app and lib added

---
+++ libs/web/angular/shared/component/README.md
@@ -0,0 +1,7 @@
+# web-angular-shared-component
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test web-angular-shared-component` to execute the unit tests.
---
+++ libs/web/angular/shared/component/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'web-angular-shared-component',
+  preset: '../../../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory:
+    '../../../../../coverage/libs/web/angular/shared/component',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+}
